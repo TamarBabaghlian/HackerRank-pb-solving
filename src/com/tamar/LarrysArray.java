0a21d768fab437ad7382eacf22d1497fb222dde7
TamarBaboghlian <t.baboghlian@gmail.com>
2020-11-27 01:30:09 +0400

    solving solving

--- src/com/tamar/LarrysArray.java
+++ src/com/tamar/LarrysArray.java
@@ -1,2 +1,32 @@
-package com.tamar;public class LarrysArray {
+package com.tamar;
+
+//https://www.hackerrank.com/challenges/larrys-array/problem
+public class LarrysArray {
+
+    // if number of swaps even YES if odd NO
+    static String larrysArray(int[] A) {
+        int n=A.length;int flag=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(A[i]<A[j]) {
+                    flag ^= 0;
+                }
+                else
+                    flag^=1;
+            }
+        }
+        if(flag==1)
+            return "YES";
+        else
+            return "NO";
+    }
+
+    public static void main(String[] args) {
+        larrysArray(new int[]{1, 6, 5, 2, 4, 3});
+        larrysArray(new int[]{3, 1, 2});
+        larrysArray(new int[]{1, 3, 4, 2});
+        larrysArray(new int[]{1, 2, 3, 5, 4});
+    }
 }
--- src/com/tamar/MakingAnagrams.java
+++ src/com/tamar/MakingAnagrams.java
@@ -1,2 +1,48 @@
-package com.tamar;public class MakingAnagrams {
+package com.tamar;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+//https://www.hackerrank.com/challenges/making-anagrams/problem
+public class MakingAnagrams {
+    static int makingAnagrams(String s1, String s2) {
+        int count = 0;
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        for (int i=0; i<s1.length(); i++) {
+            if (map1.containsKey(s1.charAt(i))) {
+                int n = map1.get(s1.charAt(i));
+                map1.put(s1.charAt(i), n+1);
+            } else map1.put(s1.charAt(i), 1);
+        }
+
+        for (int i=0; i<s2.length(); i++) {
+            if (map2.containsKey(s2.charAt(i))) {
+                int n = map2.get(s2.charAt(i));
+                map2.put(s2.charAt(i), n+1);
+            } else map2.put(s2.charAt(i), 1);
+        }
+
+        Iterator<Character> iterator = map1.keySet().iterator();
+
+        while(iterator.hasNext()){
+            Character key   = iterator.next();
+            Integer value = map1.get(key);
+            if (map2.containsKey(key)) {
+                count += Math.min(map2.get(key),  value);
+            }
+        }
+
+        return (s1.length() + s2.length())- 2*count;
+    }
+    public static void main(String[] args) {
+
+//        int result = makingAnagrams("cde", "abc");
+//        int result2 = makingAnagrams("abc", "amnop");
+        int result3 = makingAnagrams("absdjkvuahdakejfnfauhdsaavasdlkj", "djfladfhiawasdkjvalskufhafablsdkashlahdfa");
+
+    }
 }
