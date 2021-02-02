e49f34b1a3727b93d04b036c08832f55a4992c55
TamaraBaboyan <t.baboghlian@gmail.com>
2021-02-02 14:07:52 +0400

    refactoring done

--- src/com/tamar/LilysHomework.java
+++ src/com/tamar/LilysHomework.java
@@ -6,54 +6,59 @@ import java.util.*;
 public class LilysHomework {
 
     static int lilysHomework(int[] arr) {
-
         int c1 = 0;
-        int c2 = 0;
 
+        int[] arrSorted = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(arrSorted);
 
-        int[] arr1 = Arrays.copyOf(arr, arr.length);
-        int[] arr2 = Arrays.copyOf(arr, arr.length);
-        int[] arr3 = Arrays.copyOf(arr, arr.length);
-        int[] arr4 = Arrays.copyOf(arr, arr.length);
-
+        int[] arrTemp = Arrays.copyOf(arr, arr.length);
         Map<Integer, Integer> map = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
 
-        for (int i = 0;i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             map.put(arr[i], i);
-            map2.put(arr[i], i);
         }
 
-        Arrays.sort(arr1);
+        for (int i = 0; i < arr.length; i++) {
+            if (arrSorted[i] != arrTemp[i]) {
+                int index = map.get(arrSorted[i]);
+                map.put(arrSorted[i], i);
+                map.put(arrTemp[i], index);
 
-        int j =0;
-        for (int i = arr1.length-1; i>=0; i--) {
-            arr3[j] = arr1[i];
-            j++;
-        }
-
-        for(int i=0;i<arr.length;i++) {
-            if(arr1[i]!=arr2[i])
-            {
-                int index=map.get(arr1[i]);
-                map.put(arr1[i],i);
-                map.put(arr2[i],index);
-
-                int temp = arr2[i];
-                arr2[i] = arr2[index];
-                arr2[index] = temp;
+                int temp = arrTemp[i];
+                arrTemp[i] = arrTemp[index];
+                arrTemp[index] = temp;
 
                 c1++;
             }
-            if(arr3[i]!=arr4[i])
-            {
-                int index=map2.get(arr3[i]);
-                map2.put(arr3[i],i);
-                map2.put(arr4[i],index);
+        }
 
-                int temp = arr4[i];
-                arr4[i] = arr4[index];
-                arr4[index] = temp;
+        int c2 = 0;
+
+        int[] arrRev = Arrays.copyOf(arr, arr.length);
+
+        int j = 0;
+        for (int i = arrSorted.length - 1; i >= 0; i--) {
+            arrRev[j] = arrSorted[i];
+            j++;
+        }
+
+        arrTemp = Arrays.copyOf(arr, arr.length);
+
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            map2.put(arr[i], i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arrRev[i] != arrTemp[i]) {
+                int index = map2.get(arrRev[i]);
+                map2.put(arrRev[i], i);
+                map2.put(arrTemp[i], index);
+
+                int temp = arrTemp[i];
+                arrTemp[i] = arrTemp[index];
+                arrTemp[index] = temp;
 
                 c2++;
             }
@@ -63,7 +68,7 @@ public class LilysHomework {
     }
 
     public static void main(String[] args) {
-        int[] arr = {7,15,12,3};
+        int[] arr = {7, 15, 12, 3};
 //        lilysHomework(arr);
 //        lilysHomework(new int[]{2,5,3,1});
         lilysHomework(new int[]{3, 4, 2, 5, 1});
